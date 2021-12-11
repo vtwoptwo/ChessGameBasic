a8cf931f6652a8be550535ea71920881c30e00a8
fheshiki <70349016+paches00@users.noreply.github.com>
2021-12-11 18:27:33 +0100

    1

--- board.py
+++ board.py
@@ -1,15 +1,17 @@
 import pygame as pg
+import sys
 
 WIDTH = 660
 HEIGHT = 490
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GREY = (142, 142, 142)
 GREEN = (0, 128, 0)
 BROWN = (150, 75, 0)
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 
 pg.init()
-
+clock = pg.time.Clock()
 icon = pg.image.load('chess_piece_king.png')
 pg.display.set_caption('Chess')
 pg.display.set_icon(icon)
@@ -18,13 +20,18 @@ color = {True: WHITE, False: BLACK}
 current_color = True
 
 while True:
-    screen.fill(BLACK)
+    screen.fill(GREY)
     pg.draw.rect(screen, BROWN, (20, 20, 440, 440))
     for row in range(8):
         for square in range(8):
             pg.draw.rect(screen, color[current_color], ((40 + (square * 50)), 40 + (row * 50), 50, 50))
             current_color = not current_color
         current_color = not current_color
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
 
     pg.display.update()
 
