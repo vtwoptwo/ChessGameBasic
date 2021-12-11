aea1a297b2049ebcff8ad9b45bfe298a2e15a0c8
vtwoptwo <vera.prohaska@gmail.com>
2021-12-11 18:17:42 +0100

    random

---
+++ classes.py
@@ -0,0 +1,14 @@
+
+
+
+class Piece: 
+    def __init__(self,piece,color, position):
+        self.color = color
+        self.piece = piece
+        self.position = position
+
+
+# how do we arrange the chessboard? 
+
+
+    
