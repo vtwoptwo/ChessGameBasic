80a2f13ce68080cf91913ccbfc926f89205fb45c
fheshiki <70349016+paches00@users.noreply.github.com>
2021-12-11 18:15:53 +0100

    1

---
+++ board.py
@@ -0,0 +1,30 @@
+import pygame as pg
+
+WIDTH = 660
+HEIGHT = 490
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 128, 0)
+BROWN = (150, 75, 0)
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+pg.init()
+
+icon = pg.image.load('chess_piece_king.png')
+pg.display.set_caption('Chess')
+pg.display.set_icon(icon)
+
+color = {True: WHITE, False: BLACK}
+current_color = True
+
+while True:
+    screen.fill(BLACK)
+    pg.draw.rect(screen, BROWN, (20, 20, 440, 440))
+    for row in range(8):
+        for square in range(8):
+            pg.draw.rect(screen, color[current_color], ((40 + (square * 50)), 40 + (row * 50), 50, 50))
+            current_color = not current_color
+        current_color = not current_color
+
+    pg.display.update()
+
