a6400b3306f07901585cc2453c30452c8f910c14
vtwoptwo <84085010+vtwoptwo@users.noreply.github.com>
2021-12-17 23:56:09 +0100

    final

--- venv/Chess/vchessgame_main.py
+++ venv/Chess/vchessgame_main.py
@@ -1,5 +1,8 @@
 #imported libraries
 
+from types import ClassMethodDescriptorType
+from typing import ByteString
+from Chess.vchessgame_engine import TreeNode
 from vchessgame_engine import TreeNode as t 
 import pygame as pg
 import vchessgame_engine as Engine
@@ -136,41 +139,7 @@ def castling(bool, move, root):
         if a and b == None: 
             pass #do the castling 
 
-def createPIECESNUM():
-    for piece in PIECES:
-        case = {PIECES[piece][0]: piece}
-        PIECESNUM.update(case)
-    return PIECESNUM
 
-def create_array(gs):
-    numZ = []
-    for i in gs.Zombies:
-        num = PIECES[i][0]
-        print("Number:", num)
-        numZ.append(num)
-    return numZ
-
-def undoArray(numZ): 
-    strZ =[]
-    for num in numZ:
-        string = PIECESNUM[num]
-        strZ.append(string)
-    return strZ
-
-def quicksort(gs): 
-    array = create_array(gs)
-    # sort function
-    if len(array) < 2:
-        return array # One or none elements, the subarray is already sorted
-    pivot = int(len(array) / 2) # Pick the element at the middle of the array as pivot
-    pivot_value = array[pivot]
-    left = [x for x in array[0:pivot] + array[pivot+1:] if x <= pivot_value] # The left sub array with all elements smaller or equal than pivot_value
-    right = [x for x in array[0:pivot] + array[pivot+1:] if x > pivot_value] # The right subarray with all the elements larger than pivot_value
-    return quicksort(left) + [pivot_value] + quicksort(right)
-    
-def sortZombies(gs): 
-    gs.Zombies = undoArray(quicksort(gs))
-    return gs.Zombies
 
 
 
@@ -248,6 +217,8 @@ def main():
                 if WIDTH//100 <= posb[0] <= WIDTH//100 + 150 and HEIGHT-HEIGHT//2.5 + 70 <= posb[1] <= HEIGHT-HEIGHT//2.5 + 70 + 50:
                     print('CASTLE-ME')
 
+
+
                 #Translating pixel to row,col coord
                 pos = pg.mouse.get_pos()
                 col, row = pos[0]//SQ_SIZE , pos[1]//SQ_SIZE
@@ -317,21 +288,139 @@ def main():
 
 
 
-#within the driver code we have functions which help us manage the graphics of the game 
 
-TIME COMPLEXITIES
-
-Data Structures: 
-
-MATRIX
-for the matrix we actually change positions of pieces by having a time complexity of o(1)
-we take the x andy value separate for each predefined quare in the matrix and 
-
-                
-
-
-
-
-# if __name__ == "__main__": 
 
 main()
+
+
+
+
+# TIME COMPLEXITIES
+
+# Data Structures: 
+
+# DS A: MATRIX
+# for the matrix we actually change positions of pieces by having a time complexity of o(1)
+# we take the x and y value separate for each predefined quare in the matrix and and can thus manipulate 
+# piece movements in O(1) time
+
+# # DS B:  BINARY SEARCH TREE
+
+# The binary search tree time complexity to search for the pieces moved is O(logn)
+
+# this is quite a complex process since when we undid move we also had to remove nodes form the tree account for three different Clases
+#the node having children, the node having no children, the node had 1 child
+# # this datas structure was created to check if a king or rook has been moved or not in order to 
+# # implement a "castling functionality"
+
+# we managed to tdo this with one of the kings 
+
+# due to tiume constraints we were unable to do it for both sides of each color, but the king for each color castles with the rook closest to him 
+
+# DS C: DICTIONARY
+
+# we used dictionaries in several instances, mostly to create 
+# conversions between piece location, piece notation, piece names and piece values 
+
+# this could have easily been avoided by implementing a hash table form the beginning and individual piece
+# classes, but this was realized too late and we could not implement it in time without changing everything 
+
+# DS D: HEAP
+# the heap was created in order to apply a bubble sorting algorithm 
+# we ran into a few bugs once implemeing the heap but we think that with more time this would have been a problem which was easy to solve 
+
+# in fact we recognize that we should have used a hash function to identify the pieces, 
+
+# to be honest we had already created the hash function and were doing this 
+# but later decided against it
+
+# below you can see our implementation of the hash function 
+
+
+# PIECES = {'bR': (3, "<Surface(62x62x32 SW)>"), 'bN': (4, "<Surface(62x62x32 SW)>"), 'bB': (5, "<Surface(62x62x32 SW)>"), 'bQ': (2, "<Surface(62x62x32 SW)>"), 'bK': (1, "<Surface(62x62x32 SW)>"), 'wB': (5, "<Surface(62x62x32 SW)>"), 'wN': (4, "<Surface(62x62x32 SW)>"), 'wR': (3, "<Surface(62x62x32 SW)>"), 'wQ': (2, "<Surface(62x62x32 SW)>"), 'wK': (1, "<Surface(62x62x32 SW)>"), 'bP': (6, "<Surface(62x62x32 SW)>"), 'wP': (6, "<Surface(62x62x32 SW)>")}
+# HASHPIECESW = []
+# HASHPIECESB = []
+
+
+# def str_to_number(string):
+#   number = 0
+#   # A string in python is just a list of characters, with extra functions built around. So we can iterate over it using the index of each character.
+#   # We will use then the index of each character to compose the exponent to which we have to raise the base, 128, for each of them.
+#   for i in range(len(string)): 
+#     exponent = len(string) - 1 - i # Exponents start at len(string) - 1, and decrease one by one upt to 0.
+#     asc = ord(string[i]) # Compute the ascii number corresponding to that character at position i.
+#     number += asc * (128**exponent) # Calculate the base-128 for this character at position i, and accumulate it in number
+#   return number
+
+
+# for piece in PIECES:
+#     if piece[0] == 'w':
+#         case = (piece + str(PIECES[piece][0]))
+#         hase = str_to_number(case)
+#         HASHPIECESW.append(hase)
+#     if piece[0] == 'b':
+#         case = (piece + str(PIECES[piece][0]))
+#         hase = str_to_number(case)
+#         HASHPIECESB.append(hase)
+
+
+# print(HASHPIECESB)
+# print(HASHPIECESW)
+
+
+# ALGORITHMS: 
+
+# binary search through a BST :) 
+
+# sorting function for the heap :) 
+
+
+# Attempted applications of quicksort which has a to,e complexity of 
+# O(nlogn)
+
+# def createPIECESNUM():
+#     for piece in PIECES:
+#         case = {PIECES[piece][0]: piece}
+#         PIECESNUM.update(case)
+#     return PIECESNUM
+
+# def create_array(gs):
+#     numZ = []
+#     for i in gs.Zombies:
+#         num = PIECES[i][0]
+#         print("Number:", num)
+#         numZ.append(num)
+#     return numZ
+
+# def undoArray(numZ): 
+#     strZ =[]
+#     for num in numZ:
+#         string = PIECESNUM[num]
+#         strZ.append(string)
+#     return strZ
+
+# def quicksort(gs): 
+#     array = create_array(gs)
+#     # sort function
+#     if len(array) < 2:
+#         return array # One or none elements, the subarray is already sorted
+#     pivot = int(len(array) / 2) # Pick the element at the middle of the array as pivot
+#     pivot_value = array[pivot]
+#     left = [x for x in array[0:pivot] + array[pivot+1:] if x <= pivot_value] # The left sub array with all elements smaller or equal than pivot_value
+#     right = [x for x in array[0:pivot] + array[pivot+1:] if x > pivot_value] # The right subarray with all the elements larger than pivot_value
+#     return quicksort(left) + [pivot_value] + quicksort(right)
+    
+# def sortZombies(gs): 
+#     gs.Zombies = undoArray(quicksort(gs))
+#     return gs.Zombies
+
+
+
+#Heap 
+
+# the reason we use heap is because the heap is ideal for
+#  constant updating situations such as when a piece dies and needs 
+# to appear on the screen
+# 
+# furthermore the heap has a time complexity average case of O(1)
+#  and worst case time complexity of O(1) 
