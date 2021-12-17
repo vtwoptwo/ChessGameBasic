34871844da3b58f49469dde97ab800dc0665fa3c
ZaneReda <59884549+ZaneReda@users.noreply.github.com>
2021-12-17 23:56:24 +0100

    final

--- venv/Chess/chessgame_engineHEAP.py
+++ venv/Chess/chessgame_engineHEAP.py
@@ -1,5 +1,7 @@
 import pygame as pg
 from pygame.locals import *
+
+# We would use this graph for our reccomendation system if we had more time as of right now it is not being used
 class WGraph:
   def __init__(self):
     self.vertices = {}
@@ -21,7 +23,7 @@ class WGraph:
       print(f'{vertex} -> {edges}')
 
 
-
+# This is our main operating class, it keeps track of all our game operations
 class GameState():
     def __init__(self):
         # so far a 2d matrix
@@ -35,20 +37,23 @@ class GameState():
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
         ]
+
+        #boolean value to indicate white or black move
         self.whiteMove = True
 
-
+        # used for undoing moves
         self.movehistory = [] # implementation of a stack :) this can be used to undo a move 
                                 #if we undo a move there actually needs to be an implementation of some 
                                 #rules...movehistory, versus backtracking? does it count as a move? 
+        # keeps track of taken pieces
         self.Zombies = []
+        #our two heaps used for blitting a sorted list of captured pieces to the right side of the screen in order of value
         self.ZombiesBlack = Heap(16,max_heap)
         self.ZombiesWhite = Heap(16,max_heap)
+        # helper dicationaries for operating with the heaps
         self.PIECESW = {}
         self.PIECESB = {}
-        self.ZombiesWhite = Heap(16,max_heap)
-        self.ZombiesBlack = Heap(16,max_heap)
-     
+    # where the two dictionaries above are filled to their relationships established
     def initialize_dicts(self, dict):
           for piece in dict:
               if piece[0] == 'w':
@@ -57,7 +62,7 @@ class GameState():
               if piece[0] == 'b':
                   case = {dict[piece][0]: dict[piece][1]}
                   self.PIECESB.update(case)
-      
+    #keeps track of the updating of the heaps
     def updateHeaps(self,dict):
           self.ZombiesWhite.__init__(16,max_heap)
           self.ZombiesBlack.__init__(16,max_heap)
@@ -72,7 +77,7 @@ class GameState():
           print("White Data Heap: ",self.ZombiesWhite.data)
           print("Black Data Heap: ",self.ZombiesBlack.data)
           
-
+    # draws dead pieces
     def drawZombies(self,screen, dict): 
         wp = 0
         bp = 0 
@@ -258,21 +263,21 @@ class TreeNode:
       self.print_tree(node.left_child, level + 1)
 
   def insert(self,root,value):
-    if root is None: # We cannot insert on an empty root. The root should exist first.
+    if root is None: 
       return
-    parent = None # We hold a pointer to the node that will be the parent of the new node we are inserting
-    node = root # Also, we hold a pointer to traverse all the way through the path in which our new node will be inserted
-    new_node = TreeNode(value) # Our new node to be inserted.
-    while node is not None: # Keep traversing down until node is None. When that condition is met, parent will hold a pointer to the immediate previous node visited by the `node` variable, and thus, it will be the node where to hang our new node.
+    parent = None 
+    node = root 
+    new_node = TreeNode(value) 
+    while node is not None: 
       parent = node
-      if node.value > value: # If the value of the visited node is larger than our new value, then we should continue through the left subtree.
+      if node.value > value: 
         node = node.left_child
-      else: # Otherwise continue through the right subtree.
+      else: 
         node = node.right_child
-    # We have found the parent of our new node:
-    if parent.value > value: # If the value of the parent is larger than the new value, insert the new node at the left of `parent`
+    
+    if parent.value > value: 
       parent.left_child = new_node
-    else: # Otherwise, insert the new node at the right of the `parent`
+    else: 
       parent.right_child = new_node
 
   def rcsearch(self,node,value):
@@ -334,16 +339,9 @@ class TreeNode:
     elif(value > root.value):
         root.right_child = self.remove(root.right_child, value)
         return root
- 
-    # We reach here when root is the node
-    # to be deleted.
-     
-    # If root node is a leaf node
      
     if root.left_child is None and root.right_child is None:
           return None
- 
-    # If one of the children is empty
  
     if root.left_child is None:
         temp = root.right_child
@@ -354,12 +352,8 @@ class TreeNode:
         temp = root.left_child
         root = None
         return temp
- 
-    # If both children exist
- 
+
     pParent = root
- 
-    # Find Successor
  
     pre_parent = root.right_child
  
@@ -367,18 +361,10 @@ class TreeNode:
         pParent = pre_parent
         pre_parent = pre_parent.left_child
  
-    # Delete successor.Since successor
-    # is always left_child child of its parent
-    # we can safely make successor's right_child
-    # right_child child as left_child of its parent.
-    # If there is no pre_parent, then assign
-    # pre_parent->right_child to pParent->right_child
     if pParent != root:
         pParent.left_child = pre_parent.right_child
     else:
         pParent.right_child = pre_parent.right_child
- 
-    # Copy Successor Data to root
  
     root.value = pre_parent.value
  
@@ -386,10 +372,10 @@ class TreeNode:
     
 class Heap:
   def __init__(self, max_size, priority_func):
-    self.data = [0] * max_size # Initialize our data container array. It will be an array of size `max_size`, with all its values zeroed.
-    self.item_count = 0 # The next empty slot where to insert new elements
-    self.max_size = max_size # Keep the max_size as an upper bound for out Heap
-    self.priority = priority_func # The priority function to decide which of two elements has the higest priority.
+    self.data = [0] * max_size 
+    self.item_count = 0 
+    self.max_size = max_size 
+    self.priority = priority_func
     
   def parent_index(self, i):
     return ((i + 1) // 2) - 1
@@ -401,16 +387,16 @@ class Heap:
     return 2 * (i + 1)
     
   def insert(self, value):
-    if self.item_count == self.max_size: # Heap Overflow. Do not insert if the Heap is full
+    if self.item_count == self.max_size: 
       return
-    self.data[self.item_count] = value # Store the new element at the next empty spot.
-    self.bubble_up(self.item_count) # Then, make it bubble up while its priority is higher than that of its parents.
+    self.data[self.item_count] = value 
+    self.bubble_up(self.item_count) 
     self.item_count += 1 
 
   def extract(self):
-    if self.item_count == 0: # Heap Underflow. We cannot extract from the heap if it's empty.
+    if self.item_count == 0: 
       return
-    value = self.data[0] # The root of our heap is at pos 0 of our data array
+    value = self.data[0] 
     self.item_count -= 1
     self.data[0] = self.data[self.item_count] # Move the last element to the root of the heap
     self.bubble_down(0) # And then, make it bubble down while its priority is lower than that of its children
@@ -455,8 +441,7 @@ def min_heap(x, y):
 
 class button():
         
-        #colours for button and text
- 
+        #colours for button and text and other attributes
         button_col = (255, 255, 255)
         hover_col = (75, 225, 255)
         click_col = (50, 150, 255)
@@ -469,11 +454,13 @@ class button():
         WHITE = (255, 255, 255)
         GREEN = (0, 102, 0)
 
+        # init for button
         def __init__(self, x, y, text,screen):
             self.x = x
             self.y = y
             self.text = text
             self.screen = screen
+            #this kind of strange init part is so the button management is performed in the engine
             WIDTH = 750
             HEIGHT = 900
             BLACK = (0, 0, 0)
@@ -498,7 +485,6 @@ class button():
             button_rect = Rect(self.x, self.y, self.width, self.height)
             
             #check mouseover and clicked conditions
-            
             pg.draw.rect(self.screen, self.button_col, button_rect)
             
             #add shading to button
@@ -510,7 +496,10 @@ class button():
             text_len = text_img.get_width()
             self.screen.blit(text_img, (self.x + int(self.width / 2) - int(text_len / 2), self.y + 18))
             return action
-
+        
+        #this code is allowing the buttons to change colors when the mouse highlights over them, nice UI addition
+        # kind of costly for effeciency but for the user adds significantly
+        # much like the click locations these intervals just define where the mouse needs to be for the button to turn gree or white respectively 
         def buttonColorManage(self,pos,undoButton,resetButton,quitButton,castleButton):
                 if self.WIDTH //100 <= pos[0] <= self.WIDTH//100 + 150 and self.HEIGHT-self.HEIGHT//2.5 <= pos[1] <= self.HEIGHT-self.HEIGHT//2.5 + 50:
                     undoButton.button_col = (self.GREEN)
@@ -559,3 +548,133 @@ class button():
                     quitButton.draw_button()
                     castleButton.button_col = (self.WHITE)
                     castleButton.draw_button()
+
+# TIME COMPLEXITIES
+
+# Data Structures: 
+
+# DS A: MATRIX
+# for the matrix we actually change positions of pieces by having a time complexity of o(1)
+# we take the x and y value separate for each predefined quare in the matrix and and can thus manipulate 
+# piece movements in O(1) time
+
+# # DS B:  BINARY SEARCH TREE
+
+# The binary search tree time complexity to search for the pieces moved is O(logn)
+
+# this is quite a complex process since when we undid move we also had to remove nodes form the tree account for three different Clases
+#the node having children, the node having no children, the node had 1 child
+# # this datas structure was created to check if a king or rook has been moved or not in order to 
+# # implement a "castling functionality"
+
+# we managed to tdo this with one of the kings 
+
+# due to tiume constraints we were unable to do it for both sides of each color, but the king for each color castles with the rook closest to him 
+
+# DS C: DICTIONARY
+
+# we used dictionaries in several instances, mostly to create 
+# conversions between piece location, piece notation, piece names and piece values 
+
+# this could have easily been avoided by implementing a hash table form the beginning and individual piece
+# classes, but this was realized too late and we could not implement it in time without changing everything 
+
+# DS D: HEAP
+# the heap was created in order to apply a bubble sorting algorithm 
+# we ran into a few bugs once implemeing the heap but we think that with more time this would have been a problem which was easy to solve 
+
+# in fact we recognize that we should have used a hash function to identify the pieces, 
+
+# to be honest we had already created the hash function and were doing this 
+# but later decided against it
+
+# below you can see our implementation of the hash function 
+
+
+# PIECES = {'bR': (3, "<Surface(62x62x32 SW)>"), 'bN': (4, "<Surface(62x62x32 SW)>"), 'bB': (5, "<Surface(62x62x32 SW)>"), 'bQ': (2, "<Surface(62x62x32 SW)>"), 'bK': (1, "<Surface(62x62x32 SW)>"), 'wB': (5, "<Surface(62x62x32 SW)>"), 'wN': (4, "<Surface(62x62x32 SW)>"), 'wR': (3, "<Surface(62x62x32 SW)>"), 'wQ': (2, "<Surface(62x62x32 SW)>"), 'wK': (1, "<Surface(62x62x32 SW)>"), 'bP': (6, "<Surface(62x62x32 SW)>"), 'wP': (6, "<Surface(62x62x32 SW)>")}
+# HASHPIECESW = []
+# HASHPIECESB = []
+
+
+# def str_to_number(string):
+#   number = 0
+#   # A string in python is just a list of characters, with extra functions built around. So we can iterate over it using the index of each character.
+#   # We will use then the index of each character to compose the exponent to which we have to raise the base, 128, for each of them.
+#   for i in range(len(string)): 
+#     exponent = len(string) - 1 - i # Exponents start at len(string) - 1, and decrease one by one upt to 0.
+#     asc = ord(string[i]) # Compute the ascii number corresponding to that character at position i.
+#     number += asc * (128**exponent) # Calculate the base-128 for this character at position i, and accumulate it in number
+#   return number
+
+
+# for piece in PIECES:
+#     if piece[0] == 'w':
+#         case = (piece + str(PIECES[piece][0]))
+#         hase = str_to_number(case)
+#         HASHPIECESW.append(hase)
+#     if piece[0] == 'b':
+#         case = (piece + str(PIECES[piece][0]))
+#         hase = str_to_number(case)
+#         HASHPIECESB.append(hase)
+
+
+# print(HASHPIECESB)
+# print(HASHPIECESW)
+
+
+# ALGORITHMS: 
+
+# binary search through a BST :) 
+
+# sorting function for the heap :) 
+
+
+# Attempted applications of quicksort which has a to,e complexity of 
+# O(nlogn)
+
+# def createPIECESNUM():
+#     for piece in PIECES:
+#         case = {PIECES[piece][0]: piece}
+#         PIECESNUM.update(case)
+#     return PIECESNUM
+
+# def create_array(gs):
+#     numZ = []
+#     for i in gs.Zombies:
+#         num = PIECES[i][0]
+#         print("Number:", num)
+#         numZ.append(num)
+#     return numZ
+
+# def undoArray(numZ): 
+#     strZ =[]
+#     for num in numZ:
+#         string = PIECESNUM[num]
+#         strZ.append(string)
+#     return strZ
+
+# def quicksort(gs): 
+#     array = create_array(gs)
+#     # sort function
+#     if len(array) < 2:
+#         return array # One or none elements, the subarray is already sorted
+#     pivot = int(len(array) / 2) # Pick the element at the middle of the array as pivot
+#     pivot_value = array[pivot]
+#     left = [x for x in array[0:pivot] + array[pivot+1:] if x <= pivot_value] # The left sub array with all elements smaller or equal than pivot_value
+#     right = [x for x in array[0:pivot] + array[pivot+1:] if x > pivot_value] # The right subarray with all the elements larger than pivot_value
+#     return quicksort(left) + [pivot_value] + quicksort(right)
+    
+# def sortZombies(gs): 
+#     gs.Zombies = undoArray(quicksort(gs))
+#     return gs.Zombies
+
+
+
+#Heap 
+
+# the reason we use heap is because the heap is ideal for
+#  constant updating situations such as when a piece dies and needs 
+# to appear on the screen
+# 
+# furthermore the heap has a time complexity average case of O(1)
+#  and worst case time complexity of O(1) 
